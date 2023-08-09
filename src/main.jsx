5beeb01e1ce45df9ee9d5cc1d8ed4420742b2cb6
DVorortnoy <dimchik505@gmail.com>
2023-08-09 15:53:19 +0300

    add scss

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { persistor, store } from './redux/store';
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import "./index.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import App from "../src/App";
+import { BrowserRouter, HashRouter} from "react-router-dom";
+
+import './index.scss';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+     {/* <PersistGate loading={null} persistor={persistor}> */}
+     <BrowserRouter basename='/rentcar' >
+        <App />
+       </BrowserRouter>
+     {/* </PersistGate> */}
+    </Provider>
+);
